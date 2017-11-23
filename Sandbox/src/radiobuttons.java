03d98692a1da6eb90810405ab57ab498059c07a4
Patrick Varas <varaspj@gmail.com>
2017-11-23 14:59:55 -0500

    Updated to add Firefox support and added .getSize

--- Sandbox/src/radiobuttons.java
+++ Sandbox/src/radiobuttons.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -13,8 +15,11 @@ public class radiobuttons {
 			//System.setProperty("webdriver.chrome.driver", "/Users/patrickvaras/Downloads/chromedriver");
 			//WebDriver driver=new ChromeDriver();
 			//Using Safari driver due to limitations with Chrome driver and excessive DIVs
-			WebDriver driver=new SafariDriver();
+			//WebDriver driver=new SafariDriver();
+			System.setProperty("webdriver.gecko.driver", "/Users/patrickvaras/Downloads/geckodriver");
+			WebDriver driver=new FirefoxDriver();
 			driver.get("http://echoecho.com/htmlforms10.htm");
+			driver.manage().window().maximize();
 			System.out.println("Test site invoked - Radio Buttons");
 			System.out.println("         ");
 			
@@ -26,7 +31,13 @@ public class radiobuttons {
 			
 			//Selecting radio buttons	
 			driver.findElement(By.xpath("//input[@value='Milk']")).click();
+			//driver.findElement(By.xpath("//input[@name='group1']")).getSize();
+			
 			System.out.println("Milk Selected");
+			
+			//Count number of checkboxes in a group
+			System.out.println(driver.findElement(By.xpath("//input[@name='group1']")).getSize());
+			
 
 	}
 

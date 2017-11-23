d7969b4cf503f76267218632071a943fd779b4a4
Patrick Varas <varaspj@gmail.com>
2017-11-23 01:14:02 -0500

    third try

--- Sandbox/src/spicejetnav.java
+++ Sandbox/src/spicejetnav.java
@@ -13,11 +13,13 @@
 			public static void main(String[] args) {
 				// TODO Auto-generated method stub
 				
+				//Changes
+				
 				System.setProperty("webdriver.chrome.driver", "/Users/patrickvaras/Downloads/chromedriver");
 				WebDriver driver=new ChromeDriver();
 				driver.get("http://www.spicejet.com/");
 				System.out.println("Test site invoked");
-				System.out.println("         ");
+				System.out.println("          ");
 				
 				//Wait for first dropdown to become available before clicking
 				WebDriverWait wait = new WebDriverWait(driver, 40);
